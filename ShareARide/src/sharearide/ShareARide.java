94c3d7086ac30dce2d63e4d0578645fda55ac90e
DgjJqSBMk <emirjolaplaku@htl-shkoder.com>
2016-10-04 22:35:10 +0200

    changes

--- ShareARide/src/sharearide/ShareARide.java
+++ ShareARide/src/sharearide/ShareARide.java
@@ -15,7 +15,7 @@ public class ShareARide {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("emi");
+        System.out.println("test");
     }
     
 }
